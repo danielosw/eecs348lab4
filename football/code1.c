7c50c9d1d0e3473233046f35f19d703651e5b55a
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-04 16:52:51 -0500

    bump

---
+++ football/code1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int score;
+	scanf("%d",&score);
+	int cool [5] = {0,0,0,0,0};
+	while (score > 1) {
+	
+	for (int plays = 0; plays < 4; plays++) {
+	   if(plays==4){
+	      if (score-8>=0){
+		  for (int i = 0; i < count; i++) {
+		  }
+		  
+	      }
+	   } 
+	}
+	scanf("%d",&score);	
+	}
+	return 1;
+}
