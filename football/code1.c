808c32c4f4f9d7303a079b29287b409bfc68400f
danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-06 17:04:45 -0500

    made it actually work. Currently it repeats a lot but ????

--- football/code1.c
+++ football/code1.c
@@ -1,21 +1,86 @@
 #include <stdio.h>
+void recuse(int score, int eight, int seven, int six, int three, int two, int realscore){
+int cool [5] = {0,0,0,0,0};
+
+	for (int plays = 4; plays >= 0; plays--) {
+		
+	   if(plays==4){
+		  for (; eight>0; eight--) {
+			
+		      score-=8;
+		      cool[0]+=1;
+		  }
+	   }
+	   else if(plays==3){
+	       for(; seven>0; seven--){
+			
+		   score-=7;
+		   cool[1]+=1;
+	       }
+	   }
+	   else if(plays==2){
+	       for(; six>0; six--){
+			
+		   score-=6;
+		   cool[2]+=1;
+	       }
+	   }
+	   else if(plays==1){
+	       for(; three>0;three--){
+			
+		   score-=3;
+		   cool[3]+=1;
+	       }
+	   }
+	   else if(plays==0){
+	       for(; two>0;two--){
+			
+		   score-=2;
+		   cool[4]+=1;
+	       }
+	   }
+	    
+	
+	}
+	if(score>=0){
+	if(score==0){
+	    printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n",cool[0],cool[1],cool[2],cool[3],cool[4]);
+	}
+	else{
+	   if(score-8>=0){
+	       recuse(realscore,cool[0]+1,cool[1],cool[2],cool[3],cool[4], realscore);
+	   }
+	   if(score-7>=0){
+	       recuse(realscore,cool[0],cool[1]+1,cool[2],cool[3],cool[4], realscore);
+			
+	   }
+	   if(score-6>=0){
+		recuse(realscore,cool[0],cool[1],cool[2]+1,cool[3],cool[4], realscore);
+		
+	   }
+	   if(score-3>=0){
+	       recuse(realscore,cool[0],cool[1],cool[2],cool[3]+1,cool[4], realscore);
+			
+	   }
+	    if(score-2>=0){
+	       recuse(realscore,cool[0],cool[1],cool[2],cool[3],cool[4]+1, realscore);
+			
+		   
+	   }
+	   
+	}}
+
+}
 int main(int argc, char *argv[])
 {
 	int score;
-	scanf("%d",&score);
-	int cool [5] = {0,0,0,0,0};
-	while (score > 1) {
 	
-	for (int plays = 0; plays < 4; plays++) {
-	   if(plays==4){
-	      if (score-8>=0){
-		  for (int i = 0; i < count; i++) {
-		  }
-		  
-	      }
-	   } 
-	}
-	scanf("%d",&score);	
+	scanf("%d",&score);
+	while(score>1){
+	    recuse(score,0,0,0,0,0, score);
+	    scanf("%d",&score);
 	}
 	return 1;
 }
+
+
